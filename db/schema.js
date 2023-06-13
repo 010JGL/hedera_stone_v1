9f14f0f1461d475180b371c82eb8787580b5cf87
Jeremy <010JGL@GMAIL.COM>
2023-06-13 16:37:59 -0400

    changing DB helpers to deploy

--- db/schema.js
+++ db/schema.js
@@ -1,23 +1,10 @@
 import { sql } from "@vercel/postgres";
 
-const { nfts } = await sql`DROP TABLE IF EXISTS nfts CASCADE;
-  CREATE TABLE nfts (  
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(255) NOT NULL,
-  image_url VARCHAR(255) NOT NULL,
-  threed_image_url VARCHAR(255) NOT NULL,
-  price INTEGER NOT NULL,
-  creator VARCHAR(255) NOT NULL,
-  description TEXT NOT NULL,
-  date TIMESTAMP
-  );`
 
-  
-  import { db } from "@vercel/postgres";
 
-const client = await db.connect();
-await client.sql`DROP TABLE IF EXISTS nfts CASCADE;`;
-await client.sql`CREATE TABLE nfts (
+// nfts
+`DROP TABLE IF EXISTS nfts CASCADE;`;
+`CREATE TABLE nfts (
     id SERIAL PRIMARY KEY,
     words VARCHAR(255) NOT NULL,
     firstname VARCHAR(255) NOT NULL,
