e00f73da5ad1ae1e8bde72c01ddcb6d0a39cc7d3
Jeremy <010JGL@GMAIL.COM>
2023-06-13 16:35:27 -0400

    trying to fix deploy

--- db/schema.js
+++ db/schema.js
@@ -11,3 +11,23 @@ const { nfts } = await sql`DROP TABLE IF EXISTS nfts CASCADE;
   description TEXT NOT NULL,
   date TIMESTAMP
   );`
+
+  
+  import { db } from "@vercel/postgres";
+
+const client = await db.connect();
+await client.sql`DROP TABLE IF EXISTS nfts CASCADE;`;
+await client.sql`CREATE TABLE nfts (
+    id SERIAL PRIMARY KEY,
+    words VARCHAR(255) NOT NULL,
+    firstname VARCHAR(255) NOT NULL,
+    middlename VARCHAR(255) NOT NULL,
+    surname VARCHAR(255) NOT NULL,
+    datebirth VARCHAR(255) NOT NULL,
+    datedied VARCHAR(255) NOT NULL,
+    country VARCHAR(255) NOT NULL,
+    city VARCHAR(255) NOT NULL,
+    cemeteryname VARCHAR(255) NOT NULL,
+    extras VARCHAR(255) NOT NULL,
+    date TIMESTAMP
+    );`;
