59d78e4f552d8055494f69baefb0e063b3e6c07a
Jay <jake.hallslm@gmail.com>
2023-06-24 17:40:14 +0100

    fix transaction

--- app/api/buy/route.js
+++ app/api/buy/route.js
@@ -1,22 +1,30 @@
 import { NextResponse } from "next/server";
-import { AccountId, TransactionId, TransferTransaction } from "@hashgraph/sdk";
+import {
+  AccountId,
+  TransactionId,
+  TransferTransaction,
+  PrivateKey,
+} from "@hashgraph/sdk";
 
 const HS_PRICE = 100;
 const seller = process.env.HS_TREASURY;
-const key = process.env.KEY;
+const key = process.env.HS_PRIVATE_KEY;
 
 export async function POST(request) {
+  console.log("Private key", key);
   const res = await request.json();
-  const { tokenId, buyer } = res;
+  const { tokenId, buyer, serial } = res;
+
+  console.log("Buyer: ", buyer);
 
   const transaction = await new TransferTransaction()
-    .addNftTransfer(tokenId, serial, buyer, seller)
+    .addNftTransfer(tokenId, serial, seller, buyer)
     .addHbarTransfer(buyer, -HS_PRICE)
     .addHbarTransfer(seller, HS_PRICE)
-    .setTransactionId(TransactionId.generate(buyer))
+    .setTransactionId(TransactionId.generate(seller))
     .setNodeAccountIds([new AccountId(3)])
     .freeze()
-    .sign(key);
+    .sign(PrivateKey.fromString(key));
 
   const bytes = transaction.toBytes();
   return NextResponse.json({ bytes });
