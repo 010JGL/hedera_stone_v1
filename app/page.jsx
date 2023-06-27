d30b01c9983b0f2435909aa3f5149cc567b0537d
Jeremy <010JGL@GMAIL.COM>
2023-06-26 23:08:36 -0400

    added logo on homepage and footer

--- app/page.jsx
+++ app/page.jsx
@@ -2,11 +2,21 @@
 import Button from "react-bootstrap/Button";
 import "bootstrap/dist/css/bootstrap.css";
 import { NextUIProvider } from "@nextui-org/react";
+import Link from "next/link";
+import logo from "../public/images/HederaStoneLogo.jpg";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <NextUIProvider>
       <main className="homepage">
+      <ul className="nav-container">
+      <div className="nav-logo">
+        <Link href="/">
+          <Image src={logo} width={100} height={100} alt="HederaStone Logo" />
+        </Link>
+      </div>
+      </ul>
         <h1 className="title">HederaStone Temporary Page</h1>
         <div className="intro">
           Click here to experience the first draft CTE website.
@@ -25,6 +35,7 @@ export default function Home() {
           </Button>{" "}
         </div>
       </main>
+      <div className="footer-box"></div>
     </NextUIProvider>
   );
 }
